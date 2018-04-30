d72b131876c1b10e8161b4455ae136e5412f3c4e
Sergio2694 <sergdios09@gmail.com>
2018-04-30 15:52:52 -0600

    guarda

---
+++ app/src/main/java/com/sergio_pc/proyectoandroid/Hola.java
@@ -0,0 +1,13 @@
+package com.sergio_pc.proyectoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hola extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filtro);
+    }
+}
---
+++ app/src/main/java/com/sergio_pc/proyectoandroid/lugar.java
@@ -0,0 +1,13 @@
+package com.sergio_pc.proyectoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lugar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lugar);
+    }
+}
